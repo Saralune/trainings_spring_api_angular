3149af917932b297a81eb9c232fc28386052455d
Saralune <sarahlef59@hotmail.fr>
2022-07-22 12:10:44 +0200

    add auth files

---
+++ src/app/services/authenticate.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user.model';
+import { TrainingsService } from './trainings.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+  private users = [
+    {email:'saralune@mail.com'   , password:'123' , roles:['ADMIN','USER']},
+    {email:'user@mail.com'  , password:'123'  , roles:['USER']},    
+  ];
+  userConnected : User = new User("","",[]);
+
+  constructor(private apiService : TrainingsService) { }
+
+  //renvoi l'utilisateur en locale storage s'il existe sinon un client vide
+  getUser(){
+    let user = localStorage.getItem('user');    
+    if(user){ //si j'ai déjà un utilisateur en LS, c'est que je suis connecté
+      this.userConnected = JSON.parse(atob(user));    // décryptage
+    }
+    return this.userConnected;
+  }
+
+  login(email: string, password: string) {
+    let connected : boolean = false;
+    this.users.forEach( (user) => {
+        if((user.email == email) && (user.password == password)){
+          connected = true;
+          this.userConnected = user;
+          localStorage.setItem('user',btoa(JSON.stringify(user)));  //cryptage des données avant stockage en LS
+        }
+    });
+    return connected;
+  }
+
+  isConnected() {
+    return localStorage.getItem('user') != null; 
+  }
+
+  deconnected() {
+    localStorage.removeItem('user');
+    this.userConnected = new User("","",[]);
+  }
+
+  isAdmin() {
+    let user = this.getUser();
+    if(user.roles.length > 0){
+      if(user.roles.indexOf('ADMIN') > -1)  return true;
+    }
+    return false;
+  }
+
+}
